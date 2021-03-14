7fa3f94f0685029b6d89aa4c1502ebf0885b5dea
lucijajagnjic <ljagnjic@pmfst.hr>
2021-03-14 20:33:02 +0100

    Ispravak 5. zadatka.

--- Vjezbe_1/zadatak5.py
+++ Vjezbe_1/zadatak5.py
@@ -3,13 +3,13 @@ def odrediPravac(x1,y1,x2,y2,znak):
     #znak kao odabir hoce li biti slika ili znak
     xKoordinate = [x1,x2]
     yKoordinate = [y1,y2]
-    plt.plot(xkoordinate,yKoordinate)
+    plt.plot(xKoordinate,yKoordinate)
     if znak == 0:
         plt.show()
         #prikazuje na ekranu
     elif znak == 1:
         ime = input("Unesite ime slike: ")
-        plt.savefig(f'{ime}.pdf')
+        fig.savefig('ime.png')
         #sprema kao pdf
 
-odrediPravac(1,2,6,5)
+odrediPravac(1,2,6,5,4)
