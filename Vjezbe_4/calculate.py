13244ff9a1676d22f9d7d8b4f3d7c2ee0b3ffb42
lucijajagnjic <ljagnjic@pmfst.hr>
2021-04-08 22:36:22 +0200

    Vjezbe 4, dodatak/ispravak

--- Vjezbe_4/calculate.py
+++ Vjezbe_4/calculate.py
@@ -2,7 +2,7 @@ import calculus
 import math
 import matplotlib.pyplot as plt
 
-h = 0.0002
+h = 0.1
 
 def f1(x):
     return x*x-2*x
@@ -13,11 +13,61 @@ def f2(x):
 def f3(x):
     return 5*x**3 - 2*x**2 + 2*x-3
 
+def f4(x):
+    return 2*x**2 + 3
+
+def der_f3(x):
+    return 15*x**2 - 4*x + 2
+
+def f5(x):
+    return 2*(x**3/3) + 3*x
+
 print(calculus.derivacija(f1,2,h))
 print(calculus.derivacija(f2,2,h))
 
-lista3,lista4 = calculus.deriviranje(f3,-2,2,h)
+lista1,lista2 = calculus.deriviranje(f3,-2,2,h)
+plt.plot(lista1,lista2,".")
+plt.xlabel('f(x)')
+plt.ylabel('x')
+
+a = -2
+b = 2
+lista3 = lista1.copy()
+lista4 = []
+while a < b:
+    y = der_f3(a)
+    lista4.append(y)
+    a += h
+
 plt.plot(lista3,lista4)
-lista1,lista2 = calculus.deriviranje(f3,-2,2,0.1)
-plt.scatter(lista1,lista2)
+plt.show()
+
+integral = calculus.integriranje(f4,0,1,3)
+print(integral)
+result = calculus.trapezium(calculus.f4,0,1,3)
+print("Integral: {}.format(result)")
+
+analyt = calculus.analytic(calculus.f5,0,1)
+plt.axhline(y=analyt)
+
+xX = []
+gY = []
+dY = []
+dots = []
+x = 0
+
+for i in range(20):
+    x += 50
+    xX.append(x)
+    integral = calculus.integriranje(calculus.f4, 0, 1, x)
+    result = calculus.trapezium(calculus.f4, 0, 1, x)
+    gY.append(integral)
+    dY.append(integral)
+    dots.append(result)
+plt.plot(xX, gY, ".")
+plt.plot(xX, dY, ".")
+plt.plot(xX, dots, ".")
+plt.ylabel('Integral')
+plt.xlabel('N steps')
+plt.title("Numerička integracija  $f(x) = 5x^3 - 2x^2 + 2x - 3$")
 plt.show()
--- Vjezbe_4/calculus.py
+++ Vjezbe_4/calculus.py
@@ -1,18 +1,63 @@
 import math 
 import numpy as np 
+import matplotlib.pyplot as plt
 
 def derivacija(func,x,h):
-    d = (func(h+x)-func(x-h))/(2*h)
-    return d
+    d3 = (func(h+x)-func(x-h))/(2*h)
+    return d3
 
-def deriviranje(func,x1,x2,h):
+def derivacija2 (func,x,h):
+    d2 = ((func(x))-(func(x-h))) / h
+
+
+def deriviranje(func,x1,x2,h,m):
     dlista = []
     xlista = np.arange(x1,x2,h)
 
-    for x in xlista:
-        d = derivacija(func,x,h)
-        dlista.append(d)
+    if m == 3:
+        for x in xlista:
+            d = derivacija(func,x,h)
+            dlista.append(d)
+
+        elif m == 2:
+            for x in xlista:
+                d = derivacija2(func,x,h)
+                dlista.append(d) 
 
     return xlista, dlista
 
+def integriranje(func, a, b, N):
+    dx = (float(b)-float(a)) / float(N)
+    print(dx)
+    gX = a + dx
+    dX = a
+    gornjaMeda = 0
+    donjaMeda = 0
 
+    for i in range(N):
+        if abs(func(gX)) >= abs(func(dX)):
+            gornjaMeda += func(gX)*dx
+            donjaMeda += func(dX)*dx
+        else:
+            gornjaMeda += func(dX)*dx
+            donjaMeda += func(gX)*dx
+        gX += dx
+        dX += dx
+    return gornjaMeda, donjaMeda
+
+def trapezium(func,a,b,N):
+    dx = (float(b)-float(a)) / float(N)
+    gX = a + dx
+    dX = a
+    I = 0.0
+    for i in range(N):
+        I += ((func(gX)+func(dX))/2)*dx
+        gX += dx
+        dX += dx
+    
+    return I
+
+def analytic(func,x1,x2):
+    a = func(x2) - func(x1)
+
+    return a
